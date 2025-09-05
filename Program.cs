2f2eb98172bff93b9b1522e21eec08e2d6d0e781
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-09-05 18:24:34 +0300

    initiate project with clean architecture folder structure

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ATBS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=================================");
+        Console.WriteLine("   Airport Ticket Booking System  ");
+        Console.WriteLine("=================================");
+        Console.WriteLine("1. Passenger");
+        Console.WriteLine("2. Manager");
+        Console.WriteLine("0. Exit");
+    }
+}
