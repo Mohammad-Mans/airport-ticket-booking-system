e4b88579a1b070a08febbe15ba20d9ce469215b0
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-09-10 01:03:25 +0300

    implement importing flights csv file in manager menu

--- API/ConsoleUI/ManagerMenu.cs
+++ API/ConsoleUI/ManagerMenu.cs
@@ -1,6 +1,9 @@
+using ATBS.Domain.Interfaces;
+using ATBS.Utils;
+
 namespace ATBS.API.ConsoleUI;
 
-public class ManagerMenu : BaseMenu
+public class ManagerMenu(IFlightService flightService) : BaseMenu
 {
     protected override void DisplayMenu()
     {
@@ -17,13 +20,34 @@ public class ManagerMenu : BaseMenu
             // case "1":
             //     await FilterBookingsAsync();
             //     return true;
-            // case "2":
-            //     await UploadFlightsFromCsvAsync();
-            //     return true;
-            // default:
-            //     return false;
+            case "2":
+                await ImportFlightsAsync();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private async Task ImportFlightsAsync()
+    {
+        var path = FilePaths.ManagerImportFlightsPath;
+        try
+        {
+            var result = await flightService.ImportFromCsvAsync(path);
+
+            Console.WriteLine($"\nProcessed: {result.RowsProcessed}");
+            if (result.Errors.Count > 0)
+            {
+                Console.WriteLine("\nErrors:");
+                foreach (var (row, err) in result.Errors)
+                    Console.WriteLine($"Row {row}: {err}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nImport failed: {ex.Message}");
         }
 
-        return true;
+        WaitForKeyPress();
     }
 }
