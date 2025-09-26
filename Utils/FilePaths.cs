86b20d29080fcd804951e2fb5824d4c76e317c4d
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-09-26 16:09:55 +0300

    Remove unnecessary comment

--- Utils/FilePaths.cs
+++ Utils/FilePaths.cs
@@ -8,11 +8,6 @@ public static class FilePaths
     public static readonly string BookingFilePath = GetFilePath("Bookings");
     public static readonly string ManagerImportFlightsPath = GetImportPath("FlightsFromManager");
 
-
-    // Build an absolute path to Seed/<fileName>.csv under the *project* folder.
-    // When running the project, the working directory is usually:
-    //   <project>\bin\<config>\netX.Y\
-    // So we walk up three directories to reach the project root, then append "Seed/<file>.csv".
     private static string GetFilePath(string fileName)
     {
         var parentPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.ToString();
